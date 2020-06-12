22ef15e275195de2b3e41b7df18b3c6a06ab8b4b
Ritwick DSouza <ritwick.dsouza@dubizzle.com>
2020-06-12 12:02:08 +0400

    Use redis channels and make consumers aync

--- django_channels/settings.py
+++ django_channels/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 
 # Channels
 ASGI_APPLICATION = 'django_channels.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
